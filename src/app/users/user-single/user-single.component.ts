695a09444c2186cd699377b9e47f08367a5b806e
TazinZari <43027238+TazinZari@users.noreply.github.com>
2021-05-29 17:41:25 +0530

    Add files via upload

---
+++ src/app/users/user-single/user-single.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../core/services/user.service';
+
+@Component({
+  selector: 'app-user-single',
+  template: `
+    <section class="section">
+    <div class="container">
+  
+      <div class="card" *ngIf="user">
+        <img [src]="user.avatar_url">
+        <h2>Name: {{ user.login }}</h2>
+        <h2>Company: {{ user.company }}</h2>
+        <h2>Email: {{ user.name }}</h2>
+        <h2>Location: {{ user.location }}</h2>
+      </div>
+
+    </div>
+    </section>
+  `,
+  styles: []
+})
+export class UserSingleComponent implements OnInit {
+  user; // property to hold our user when we eventually grab them from github api
+
+  // inject the things we need
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  // watch the route parameters for changes
+  // every time it changes (or on first load), go get a user from userservice
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const username = params['username'];
+      this.userService
+          .getUser(username)
+          .subscribe(user => this.user = user);
+    });
+  }
+}
